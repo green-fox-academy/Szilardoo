3f9b013cf04c31bf12faa545eefb2690ce069021
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-03-21 14:17:16 +0100

    Odd and even exercise finished, some midified

--- Week2/day2/average-of-input.py
+++ Week2/day2/average-of-input.py
@@ -2,9 +2,9 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
-a = input("First number: ")
-b = input("Secound number: ")
-c = input("Third number: ")
-d = input("Fourth number: ")
-e = input("Fifth number: ")
-print("Sum: " , int(a)+int(b)+int(c)+int(d)+int(e), "Avarge: ", (int(a)+int(b)+int(c)+int(d)+int(e))/5)
+a = int(input("First number: "))
+b = int(input("Secound number: "))
+c = int(input("Third number: "))
+d = int(input("Fourth number: "))
+e = int(input("Fifth number: "))
+print("Sum: " , a+b+c+d+e, "Avarge: ", (a+b+c+d+e)/5)
