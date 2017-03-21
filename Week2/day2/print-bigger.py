fce3fe4f0b5ec2109cd30037f13855cb47deab1d
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-03-21 14:47:06 +0100

    More user input exercises finished, one midified

---
+++ Week2/day2/print-bigger.py
@@ -0,0 +1,7 @@
+# Write a program that asks for two numbers and prints the bigger one
+x = int(input("Give me a number, please: "))
+y = int(input("Give me an another number, please: "))
+if x > y:
+    print(x, "is bigger")
+else:
+    print(y, "is bigger")
