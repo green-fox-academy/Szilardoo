545fe877e7f3b9a37f212a94a15d5021c5a59173
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-03-22 09:09:49 +0100

    Fizz buzz exercise finished.

---
+++ Week2/day2/fizz-buzz.py
@@ -0,0 +1,12 @@
+# Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print “Fizz” instead of the number
+# and for the multiples of five print “Buzz”.
+# For numbers which are multiples of both three and five print “FizzBuzz”.
+for i in range(1,101):
+    print(i)
+    if i%5 !=0 and i%3 == 0:
+        print("Fizz")
+    elif i%3 !=0 and i%5 == 0:
+        print("Buzz")
+    if i % 5 == 0 and i % 3 == 0:
+        print("FizzBuzz")
