e8d56d67b9c30df47c06c0202dba1ad132dd32fb
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-03-22 13:38:22 +0100

    Adding a-character to the words

---
+++ Week2/day3/append-a_list.py
@@ -0,0 +1,9 @@
+# - Create a variable named `nimals`
+#   with the following content: `["kuty", "macs", "cic"]`
+# - Add all elements an `"a"` at the end
+
+nimals = ["kuty", "macsk", "cic"]
+for i in range(0, 3):
+    nimals[i] += 'a'
+
+print(nimals)
