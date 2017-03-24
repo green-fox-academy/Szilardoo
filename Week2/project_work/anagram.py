6245af6cef6574036c8c32123e1c4b71dba94d3c
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-03-24 17:42:29 +0100

    Some changes

--- Week2/project_work/anagram.py
+++ Week2/project_work/anagram.py
@@ -1,13 +1,8 @@
-# Create a function named is anagram following your current language's style guide. It should take two strings and return a boolean value depending on whether its an anagram or not.
-
-
-
 def anagram(string_one, string_two):
     list_string_one = list(string_one)
     list_string_one.sort()
     list_string_two = list(string_two)
     list_string_two.sort()
-
     return (list_string_one == list_string_two)
 
 
