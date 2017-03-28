6d1fe1721a79f6afc2ca13ea89085dfa72cd42dc
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-03-28 12:09:04 +0200

    Reverse lines

---
+++ Week3/Day2/crypto_2revlines/crypto_2revlines.py
@@ -0,0 +1,25 @@
+# Create a method that decrypts texts/reversed_zen_lines.txt
+myfile = open('reversed-lines.txt', 'r')
+
+
+def decrypt(file_name):
+    text = myfile.read()
+    ns = ""
+    for i in range(len(text),0,-1):
+        ns += text[i-1]
+    return ns
+
+print(decrypt(myfile))
+
+#solution 2 :
+# myfile = open('reversed-lines.txt', 'r')
+#
+#
+# def decrypt(file_name):
+#     text = myfile.read()
+#     ns = ""
+#     for i in range(len(text)):
+#         ns += text[i*-1]
+#     return ns
+#
+# print(decrypt(myfile))
