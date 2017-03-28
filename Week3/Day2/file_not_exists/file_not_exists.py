a09ceb2feeae1e8e5117bcb8b06d2918a097c556
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-03-28 16:13:56 +0200

    File not exists, line counter with function

---
+++ Week3/Day2/file_not_exists/file_not_exists.py
@@ -0,0 +1,15 @@
+# write a function that takes a filename and returns the number of lines the
+# file contains. It should return zero if the file does not exist.
+
+
+def file_not_exist(file_name):
+    try:
+        myfile = open(file_name, 'r')
+        text = myfile.readlines()
+        counter = len(text)
+        return counter
+    except FileNotFoundError:
+        return 0
+
+print(file_not_exist('file_not_exists.txt'))
+print(file_not_exist('file_not.txt'))
