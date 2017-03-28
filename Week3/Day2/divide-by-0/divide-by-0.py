111f79284666efe6357e19af45c70f9d17e133ca
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-03-28 15:49:11 +0200

    Upgraded with function

--- Week3/Day2/divide-by-0/divide-by-0.py
+++ Week3/Day2/divide-by-0/divide-by-0.py
@@ -3,12 +3,15 @@
 # and prints the result.
 # it should print "fail" if the parameter is 0
 
-while True:
-    try:
-        number = int(input("Give me a number, please: "))
-        basic_number = 10
-        result = basic_number/number
-        print(result)
-        break
-    except ZeroDivisionError:
-        print("fail")
+def divider():
+    while True:
+        try:
+            user_number = int(input("Give me a number, please: "))
+            basic_number = 10
+            result = basic_number/user_number
+            return result
+        except ZeroDivisionError:
+            print("fail")
+
+
+print(divider())
