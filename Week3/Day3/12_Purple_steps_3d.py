c0ee019152b372cc8be9b2013f3cad80cfffd5b2
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-03-29 17:07:21 +0200

    Y coordinate modified

--- Week3/Day3/12_Purple_steps_3d.py
+++ Week3/Day3/12_Purple_steps_3d.py
@@ -8,12 +8,12 @@ canvas.pack()
 # reproduce this:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/purple-steps/r3.png]
 
-x_coordinate = 5
-y_coordinate = 15
+x_coordinate = 15
+y_coordinate = 25
 
 for i in range(6):
     box = canvas.create_rectangle(x_coordinate, x_coordinate, y_coordinate, y_coordinate, fill="purple")
     x_coordinate = y_coordinate
-    y_coordinate += x_coordinate*0.7
+    y_coordinate += (x_coordinate+1)/2 +1
 
 root.mainloop()
