e75647d43ed16f791afd823147ec53d47073b1cc
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-03-30 10:32:31 +0200

    Adding numbers with recursive function

---
+++ Week3/day4/numberadder.py
@@ -0,0 +1,8 @@
+# Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+def count_down(number):
+    if number == 0:
+        return 0
+    else:
+        return number+count_down(number-1)
+
+print(count_down(6))
