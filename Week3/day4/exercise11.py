e0c5ab59b0f6256173547a0fa460b4f4d34742f3
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-03-31 08:44:13 +0200

    If conditon modified

--- Week3/day4/exercise11.py
+++ Week3/day4/exercise11.py
@@ -9,21 +9,19 @@ canvas.pack()
 
 canvas.create_rectangle(0,0,600,600, fill="Yellow")
 
-def squares(size, x, y, counter):
-    if counter == 0:
-        return None
-    else:
+def squares(size, x, y,):
+    if size > 5:
         canvas.create_line(x + size/3, y, x + size/3, y + size)
         canvas.create_line(x + size/3*2, y, x + size/3*2, y + size)
         canvas.create_line(x, y + size/3, x + size, y + size/3)
         canvas.create_line(x, y + size/3*2, x + size, y + size/3*2)
 
-        squares(size/3, x, y + size/3, counter-1)
-        squares(size/3, x + size/3, y, counter-1)
-        squares(size/3, x + size/3, y + size/3*2, counter-1)
-        squares(size/3, x + size/3*2, y + size/3, counter-1)
+        squares(size/3, x, y + size/3)
+        squares(size/3, x + size/3, y)
+        squares(size/3, x + size/3, y + size/3*2)
+        squares(size/3, x + size/3*2, y + size/3)
 
-squares(600,0,0,5)
+squares(600,0,0)
 
 
 root.mainloop()
