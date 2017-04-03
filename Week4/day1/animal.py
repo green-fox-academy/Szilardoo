dbcb50d920c08703c06356f78df0938d5ae80be4
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-04-03 13:12:15 +0200

    Animal hunger and thirst counter

---
+++ Week4/day1/animal.py
@@ -0,0 +1,33 @@
+# Create Animal class
+# Every animal has a hunger value, which is a whole number
+# Every animal has a thirst value, which is a whole number
+# when creating a new animal object these values are created with the default 50 value
+# Every animal can eat() which decreases their hunger by one
+# Every animal can drink() which decreases their thirst by one
+# Every animal can play() which increases both by one
+
+class Animal():
+
+    def __init__(self):
+        self.hunger = 50
+        self.thirst = 50
+
+    def eat(self):
+        self.hunger -= 1
+
+    def drink(self):
+        self.thirst -= 1
+
+    def play(self):
+        self.hunger += 1
+        self.thirst += 1
+
+cat = Animal()
+
+print(cat.hunger , cat.thirst)
+cat.eat()
+print(cat.hunger , cat.thirst)
+cat.drink()
+print(cat.hunger , cat.thirst)
+cat.play()
+print(cat.hunger , cat.thirst)
