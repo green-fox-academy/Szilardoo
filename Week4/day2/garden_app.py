3fbc4d7c78ec11efd32717746d9db0024a9aa6b7
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-04-04 14:05:54 +0200

    Added classes to garden app

---
+++ Week4/day2/garden_app.py
@@ -0,0 +1,44 @@
+class Garden():
+
+    def __init__(self):
+
+
+    def watering():
+        if self.current_water_amount < 5:
+            self.current_water_amount +=
+        else:
+            print("The "  "Flower doesnt need water")
+
+
+
+class Flower():
+
+    def __init__(self):
+            self.current_water_amount = 0
+            self.flower = str([])
+            self.water_40 = 40
+            self.water_70 = 70
+
+
+    def add_flower(self, type):
+        self.flower.append(type)
+
+    def watering():
+        if self.current_water_amount < 5:
+            self.current_water_amount +=
+        else:
+            for i in self.flower
+                print("The "  "Flower doesnt need water")
+
+
+class Tree():
+
+    def __init__(self):
+            self.current_water_amount = 0
+            self.tree = []
+            self.water_40 = 40
+            self.water_70 = 70
+
+
+    def add_tree(self, type):
+        self.tree.append(type)
