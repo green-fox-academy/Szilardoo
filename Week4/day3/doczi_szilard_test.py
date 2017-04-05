f73c7ee288fb15b0ce52c3a4a67aa82137818ab4
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-04-05 13:30:03 +0200

    Sum method created, and tested properly

--- Week4/day3/doczi_szilard_test.py
+++ Week4/day3/doczi_szilard_test.py
@@ -8,5 +8,29 @@ class TestApple(unittest.TestCase):
         self.assertEqual(apple.get_apple(), "apple")
 
 
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+    	num = Numbers()
+    	alist = [1, 2, 3]
+    	self.assertEqual(num.sum(alist), 6)
+
+    def test_sum(self):
+    	num = Numbers()
+    	alist = []
+    	self.assertEqual(num.sum(alist), 0)
+
+    def test_sum(self):
+    	num = Numbers()
+    	alist = [1]
+    	self.assertEqual(num.sum(alist), 1)
+
+    def test_sum(self):
+    	num = Numbers()
+    	alist = [0]
+    	self.assertEqual(num.sum(alist), 0)
+
+
+
 if __name__ == '__main__':
     unittest.main()
--- Week4/day3/doczi_szilard_work.py
+++ Week4/day3/doczi_szilard_work.py
@@ -2,3 +2,12 @@ class Apple():
 
     def get_apple(self):
         return "apple"
+
+
+class Numbers():
+
+    def sum(self,elements):
+    	if len(elements)==0:
+        	return 0
+    	else:
+        	return elements[0] + sum(elements[1:]) 
