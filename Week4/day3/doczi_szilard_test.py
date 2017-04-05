70d7af3372fc954a5d733f7db3ecfe4af4778be3
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-04-05 13:40:36 +0200

    Anagram method created, and tested properly

--- Week4/day3/doczi_szilard_test.py
+++ Week4/day3/doczi_szilard_test.py
@@ -10,27 +10,35 @@ class TestApple(unittest.TestCase):
 
 class TestSum(unittest.TestCase):
 
-    def test_sum(self):
+    def test_sum_multiple_elements(self):
     	num = Numbers()
     	alist = [1, 2, 3]
     	self.assertEqual(num.sum(alist), 6)
 
-    def test_sum(self):
+    def test_sum_empty_list(self):
     	num = Numbers()
     	alist = []
     	self.assertEqual(num.sum(alist), 0)
 
-    def test_sum(self):
+    def test_sum_one_element(self):
     	num = Numbers()
     	alist = [1]
     	self.assertEqual(num.sum(alist), 1)
 
-    def test_sum(self):
+    def test_sum_zero(self):
     	num = Numbers()
     	alist = [0]
     	self.assertEqual(num.sum(alist), 0)
 
+class TestAnagram(unittest.TestCase):
 
+    def test_anagram_true(self):
+        anagram = Anagram()
+        self.assertEqual(anagram.anagram_checker("parrot","raptor"), True)
+
+    def test_anagram_false(self):
+        anagram = Anagram()
+        self.assertEqual(anagram.anagram_checker("aaa","bbb"), False)
 
 if __name__ == '__main__':
     unittest.main()
--- Week4/day3/doczi_szilard_work.py
+++ Week4/day3/doczi_szilard_work.py
@@ -10,4 +10,13 @@ class Numbers():
     	if len(elements)==0:
         	return 0
     	else:
-        	return elements[0] + sum(elements[1:]) 
+        	return elements[0] + sum(elements[1:])
+
+class Anagram():
+
+		def anagram_checker(self, string_one, string_two):
+		    list_string_one = list(string_one)
+		    list_string_one.sort()
+		    list_string_two = list(string_two)
+		    list_string_two.sort()
+		    return (list_string_one == list_string_two)
