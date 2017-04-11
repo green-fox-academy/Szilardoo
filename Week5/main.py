23a52206ecf75052f0228dbbd21a197ca7d21ab7
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-04-11 16:18:37 +0200

    Map added, moves are added

--- Week5/main.py
+++ Week5/main.py
@@ -1,36 +1,71 @@
 from tkinter import *
 
-class GameObject(object):
-     def __init__(self):
-         self.testcharacterX = 65
-         self.testcharacterY = 0
-
-     def character(self, canvas):
-         canvas.create_rectangle(0, 0, 600, 600, fill='white')
-         self.filename = PhotoImage(file = "a.png")
-         canvas.create_image(self.testcharacterX, self.testcharacterY, anchor=NE, image=self.filename)
-
-# Create the tk environment as usual
 root = Tk()
-canvas = Canvas(root, width=600, height=600)
+canvas = Canvas(root, width=750, height=825)
 
-# Creating a character that can draw itself in a certain position
+class GameObject(object):
+    def __init__(self):
+        self.testcharacterX = 65
+        self.testcharacterY = 0
+        self.map = [
+            [1, 1, 1, 0, 1, 0, 1, 1, 1, 1],
+            [1, 1, 1, 0, 1, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 1, 0, 1, 0, 0, 1],
+            [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+            [1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+            [1, 1, 1, 0, 1, 0, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 1, 1, 1]
+            ]
+        self.draw_background()
+
+    def character(self, canvas):
+         self.main_character_down_pic = PhotoImage(file = "a.png")
+         self.main_character_left_pic = PhotoImage(file = "b.png")
+         self.main_character_up_pic = PhotoImage(file = "h.png")
+         self.main_character_right_pic = PhotoImage(file = "j.png")
+         self.hero = canvas.create_image(self.testcharacterX, self.testcharacterY, anchor=NE, image=self.main_character_down_pic)
+
+    def draw_background(self):
+        self.background_pic = PhotoImage(file = "floor.png")
+        w = 75
+        h= 75
+        for i in range(11):
+            for j in range(10):
+                    canvas.create_image(w*i, h*j, anchor=NE, image = self.background_pic)
+
+
+
+
 game = GameObject()
+class MainLoop():
 
-def on_key_press(e):
+    def on_key_press(self, e):
+        if e.keycode == 38:
+            canvas.itemconfig( game.hero, image=game.main_character_up_pic )
+            canvas.update()
+            if game.testcharacterY > 0:
+                game.testcharacterY = game.testcharacterY - 75
+        elif e.keycode == 40:
+            if game.testcharacterY < 750:
+                game.testcharacterY = game.testcharacterY + 75
+        elif e.keycode == 39:
+            canvas.itemconfig( game.hero, image=game.main_character_right_pic )
+            canvas.update()
+            if game.testcharacterX < 675:
+                game.testcharacterX = game.testcharacterX + 75
+        elif e.keycode == 37:
+            canvas.itemconfig( game.hero, image=game.main_character_left_pic )
+            canvas.update()
+            if game.testcharacterX > 0:
+                game.testcharacterX = game.testcharacterX - 75
+        game.character(canvas)
 
-    if e.keycode == 38:
-        game.testcharacterY = game.testcharacterY - 75
-    elif e.keycode == 40:
-        game.testcharacterY = game.testcharacterY + 75
-    elif e.keycode == 39:
-        game.testcharacterX = game.testcharacterX + 75
-    elif e.keycode == 37:
-        game.testcharacterX = game.testcharacterX - 75
 
-    game.character(canvas)
-
-canvas.bind("<KeyPress>", on_key_press)
+main = MainLoop()
+canvas.bind("<KeyPress>", main.on_key_press)
 canvas.pack()
 
 canvas.focus_set()
