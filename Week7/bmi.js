158095ee67eda1676f1b2db198034c7a237b846c
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-05-02 13:38:54 +0200

    Bmi calculator

---
+++ Week7/bmi.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Print the Body mass index (BMI) based on these values
+
+var massInKg = 81.2;
+var heightInM = 1.78;
+var bmi = massInKg/Math.pow(heightInM, 2);
+
+console.log(bmi)
