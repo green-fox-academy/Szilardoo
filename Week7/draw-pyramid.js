691ae7d387e72b4ce68ec218dd7cf154e399af76
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-05-02 15:52:31 +0200

    Draw pyramid upgraded

--- Week7/draw-pyramid.js
+++ Week7/draw-pyramid.js
@@ -12,16 +12,19 @@ var lineCount = 4;
 // *******
 //
 // The pyramid should have as many lines as lineCount is
-var star = "*";
-var space = " ";
+var star = '*';
+var space = ' ';
 var counter = lineCount;
+var pyramid = '';
 for (var i = 1; i <= lineCount; i++)
 {
     counter -= 1;
+    space = ' ';
     for (var h = 1; h <= counter; h++)
     {
-        process.stdout.write(space)
+        space += ' ';
     }
-    console.log(star)
+    pyramid = space + star;
+    console.log(pyramid);
     star += "**";
 }
