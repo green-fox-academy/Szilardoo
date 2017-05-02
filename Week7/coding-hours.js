780614c690cb548671f2fe8f86d5890fe12eb860
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-05-02 13:30:25 +0200

    Coding hours calculator

---
+++ Week7/coding-hours.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+
+var hours = 6;
+var weeks = 17;
+var days = 5;
+
+console.log('Hours spent with coding: ' + hours*days*weeks);
+console.log('Percentage of the coding hours: ' + (52/(24*days))*100)
