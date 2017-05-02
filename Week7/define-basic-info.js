a22c4cebdf51c959e0700edfe42e8e1e5c9496ad
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-05-02 13:35:34 +0200

    Store values in variables

---
+++ Week7/define-basic-info.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+var myName = 'Doczi Szilard';
+var myAge = 20;
+var myHeight = 1.8;
+var married = false;
