06bfe6db157c12cb3a05bf373dec8f614e6e0a59
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-05-03 14:15:35 +0200

    Diagonal matrix done

---
+++ Week7/Day3/diagonal-matrix.js
@@ -0,0 +1,30 @@
+// - Create (dynamically*) a two dimensional list
+//   with the following matrix**. Use a loop!
+//
+//   0 0 0 1
+//   0 0 1 0
+//   0 1 0 0
+//   1 0 0 0
+//
+// - Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+
+
+var size = 4;
+var size2 = size;
+var zero2 = '';
+
+for (var i = 1; i <= size; i++){
+	size2 -=1;
+	var zero = '0';
+	for (var j = 1 ; j <= size2-1; j ++){
+		zero += '0';
+	}
+	if (size2 === 0) {
+			zero = '';
+		}
+	console.log(zero + '1' + zero2);
+	zero2 += '0';
+}
