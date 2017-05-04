95994aa66b81848318a46574ca8d32e86a512c55
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-05-04 13:15:42 +0200

    Content1 exercise in js done

---
+++ Week7/Day4/content-1/content-1.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var heading = document.querySelector('head');
+
+alert(heading.innerHTML);
+
+var firstParagraph = document.getElementsByTagName('p')[0];
+
+console.log(firstParagraph.innerHTML)
+
+var secondParagraph = document.getElementsByClassName("other")[0];
+
+alert(secondParagraph.innerHTML);
+
+heading.textContent = 'New content';
+
+var lastParagraph = document.getElementsByClassName("result")[0];
+
+lastParagraph.innerHTML = firstParagraph.innerHTML;
