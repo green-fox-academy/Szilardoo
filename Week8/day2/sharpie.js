1c8974d96b43f64e1e2d5b7b645ebb6a412efbbf
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-05-09 14:15:02 +0200

    Sharpie exercise done

--- Week8/day2/sharpie.js
+++ Week8/day2/sharpie.js
@@ -1,6 +1,14 @@
-var sharpie = {
-	color: 
-	width:
-	inkAmount:
-	create: function(color, width, inkAmount=)
+function sharpie(color,width) {
+	this.color = color;
+	this.width = width;
+	this.inkAmount = 100;
 }
+
+var use = function (sharpie) {
+	sharpie.inkAmount -= sharpie.width;
+}
+
+var sharpie1 = new sharpie('black', 5)
+console.log(sharpie1)
+use(sharpie1);
+console.log(sharpie1)
