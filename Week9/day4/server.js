0f5156457f2813613483fdda487764520b924a62
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-05-18 16:22:04 +0200

    Create filers in js to mysql exercise done

--- Week9/day4/server.js
+++ Week9/day4/server.js
@@ -40,27 +40,53 @@ app.get('/bookstitles', function get(req,res) {
 
 
 
-const booksQueryAndJoin = "SELECT * FROM book_mast INNER JOIN author ON book_mast.aut_id = author.aut_id INNER JOIN category ON book_mast.cate_id = category.cate_id INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;"
+app.get('/books', function get(req, res) {
 
-app.get('/books', function get(req,res) {
-	var result = '<table><tr><th>Book title</th><th>Authors name</th><th>Category</th><th>Publishers name</th><th>Price</th></tr>';
+    var category = req.query.category;
+    var publisher = req.query.publisher;
+    var plt = req.query.plt;
+    var pgt = req.query.pgt;
+
+    var myList = [];
+
+    if(category !== undefined){
+    	myList.push(' cate_descrip = '+ '"' + category + '"');
+    }
+    if(publisher !== undefined){
+    	myList.push(' pub_name = '+ '"' + publisher + '"');
+    }
+    if(plt !== undefined){
+    	myList.push(' book_price < '+ '"' + plt + '"');
+    }
+    if(pgt !== undefined){
+    	myList.push(' book_price > '+ '"' + pgt + '"');
+    }
+
+    console.log(myList);
+
+    console.log(category)
 
 
-	conn.query(booksQueryAndJoin ,function(err,rows){
-		if( err ){
-			console.log("PARA", err.message);
-		} else {
-			rows.forEach(row => {
-				result += ('<tr><td>'+row.book_name+'</td><td>'+row.aut_name+'</td><td>'+row.cate_descrip+'</td><td>'+row.pub_name+'</td><td>'+row.book_price+'</td></tr>');
-			})
-		}
-		result += '</table>';
-		res.send(result);
-	})
+
+    var add = (myList.length > 0)?' WHERE ': '';
+    add += myList.join(' AND ');
+
+   var query = "SELECT * FROM book_mast INNER JOIN author ON book_mast.aut_id = author.aut_id INNER JOIN category ON book_mast.cate_id = category.cate_id INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id";
+
+   var result = "<table><tr><th>Book title</th><th>Author name</th><th>Category</th><th>Publisher name</th><th>Price</th></tr>";
+    conn.query(query+add+';',function(err,rows){
+        if(err) {
+            console.log("PARA", err.message);
+        } else {
+            rows.forEach(row => {
+                result += '<tr><td>' + row.book_name + '</td><td>' + row.aut_name + '</td><td>' + row.cate_descrip + '</td><td>' + row.pub_name + '</td><td>' + row.book_price + '</td></tr>';
+            });
+        }   result += '</table>'
+        res.send(result);
+    });
 })
-
 
 
 app.listen(3000,()=> {
 	console.log('server is running')
-});
+}); 
