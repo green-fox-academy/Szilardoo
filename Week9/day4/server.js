fb28f757cdfc309fbeefb9dc1f3e9cf4f889a4a3
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-05-18 13:03:54 +0200

    Create a table with mysql datas in js server

--- Week9/day4/server.js
+++ Week9/day4/server.js
@@ -20,10 +20,9 @@ const query = {
 	sql: "SELECT * FROM author WHERE country = ? AND home_city = ?;",
 	values: [country, city]
 }
-
 const booksQuery = "SELECT * FROM book_mast;";
 
-app.get('/books', function get(req,res) {
+app.get('/bookstitles', function get(req,res) {
 	var result = '<ul>';
 
 	conn.query(booksQuery ,function(err,rows){
@@ -35,6 +34,27 @@ app.get('/books', function get(req,res) {
 			})
 		}
 		result += '</ul>';
+		res.send(result);
+	})
+})
+
+
+
+const booksQueryAndJoin = "SELECT * FROM book_mast INNER JOIN author ON book_mast.aut_id = author.aut_id INNER JOIN category ON book_mast.cate_id = category.cate_id INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;"
+
+app.get('/books', function get(req,res) {
+	var result = '<table><tr><th>Book title</th><th>Authors name</th><th>Category</th><th>Publishers name</th><th>Price</th></tr>';
+
+
+	conn.query(booksQueryAndJoin ,function(err,rows){
+		if( err ){
+			console.log("PARA", err.message);
+		} else {
+			rows.forEach(row => {
+				result += ('<tr><td>'+row.book_name+'</td><td>'+row.aut_name+'</td><td>'+row.cate_descrip+'</td><td>'+row.pub_name+'</td><td>'+row.book_price+'</td></tr>');
+			})
+		}
+		result += '</table>';
 		res.send(result);
 	})
 })
