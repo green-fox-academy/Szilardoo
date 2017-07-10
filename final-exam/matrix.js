cc9b3369ce7ffee4d512b757d8235c4c95e025f7
Szilardoo <doczi.szilard@gmail.com>
2017-07-10 09:33:14 +0200

    feat(join): join function done

--- final-exam/matrix.js
+++ final-exam/matrix.js
@@ -1,19 +1,7 @@
-function drawMatrix(num){
-  const size = num;
-  let size2 = size;
-  let result = [];
-  let zero2 = '';
-
-  for (var i = 1; i <= size; i++){
-  	size2 -=1;
-    let zero = '';
-  	for (var j = 1 ; j <= size2; j ++){
-  		zero += ' 0';
-  	}
-  	result.push([zero2 + '1' + zero + '\n']);
-  	zero2 += '0 ';
+function join(array, letter) {
+  let joinedList = '';
+  for(let i = 0; i < array.length; i++){
+    joinedList += array[i];
   }
-  return result.join('');
+  return joinedList;
 }
-
-console.log(drawMatrix(5));
